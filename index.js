9b4e90ae949b5d1fd97fcf1607383083d7c20d31
zelda <shwarzbergzelda@gmail.com>
2026-06-08 00:34:34 -0400

    implement generating password functionality

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+const generatePasswordButton = document.getElementById('generate-pw-btn')
+const passwordOne = document.getElementById('password-one')
+const passwordTwo = document.getElementById('password-two')
+
+generatePasswordButton.addEventListener('click', () => {
+    passwordOne.textContent = generatePassword()
+    passwordTwo.textContent = generatePassword()
+})
+
+function generatePassword() {
+    let password = ''
+    let randomIndex = -1
+    for (let i = 0; i < 15; i++) {
+        randomIndex = Math.floor(Math.random() * characters.length)
+        password += characters[randomIndex]
+    }
+
+    return password
+}
