9a093bd145407a736e2b92b90a4b8678274eff4d
zelda <shwarzbergzelda@gmail.com>
2026-06-08 00:54:02 -0400

    add copying functionality

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 const generatePasswordButton = document.getElementById('generate-pw-btn')
 const passwordOne = document.getElementById('password-one')
 const passwordTwo = document.getElementById('password-two')
+const passwordLengthEl = document.getElementById('password-length')
 
 generatePasswordButton.addEventListener('click', () => {
-    passwordOne.textContent = generatePassword()
-    passwordTwo.textContent = generatePassword()
+    const passwordLength = Number(passwordLengthEl.value)
+    passwordOne.textContent = generatePassword(passwordLength)
+    passwordTwo.textContent = generatePassword(passwordLength)
 })
 
-function generatePassword() {
+passwordOne.addEventListener('click', copyPassword)
+passwordTwo.addEventListener('click', copyPassword)
+
+function copyPassword(e) {
+    navigator.clipboard.writeText(e.target.textContent)
+    
+    const originalText = e.target.textContent
+    e.target.textContent = 'Copied!'
+
+    setTimeout(() => {
+        e.target.textContent = originalText
+    }, 1000)
+
+}
+
+function generatePassword(passwordLength) {
     let password = ''
     let randomIndex = -1
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         randomIndex = Math.floor(Math.random() * characters.length)
         password += characters[randomIndex]
     }
